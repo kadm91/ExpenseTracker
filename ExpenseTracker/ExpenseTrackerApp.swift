53ccf5c1283de8304af16e06d88aa99bc27d36ca
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-02-19 20:40:59 -0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Kevin Martinez on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
